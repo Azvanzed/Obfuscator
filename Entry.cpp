df92c9acc5883975915becb48746ab7108e01c1f
Inshia Budka <107370797+Azvanzed@users.noreply.github.com>
2023-02-06 23:47:20 +0100

    gui fixes

--- Entry.cpp
+++ Entry.cpp
@@ -37,89 +37,41 @@
 
 VOID onDrawCallback()
 {
-	static BOOLEAN isFirstFrame{ TRUE }, justRanObfuscator{ FALSE };
+	static BOOLEAN isFirstFrame{ TRUE };
 	if (isFirstFrame)
 	{
-		ImGui::SetNextWindowPos( { ( float )( ( GetSystemMetrics(SM_CXSCREEN) / 2 ) - (475 / 2)),
-			(float)( ( GetSystemMetrics(SM_CYSCREEN) / 2 ) - (535 / 2) )});
-		ImGui::SetNextWindowSize({ 475, 535 });
+		ImGui::SetNextWindowPos({ (float)((GetSystemMetrics(SM_CXSCREEN) / 2) - (475 / 2)),
+			(float)((GetSystemMetrics(SM_CYSCREEN) / 2) - (140 / 2)) });
+		ImGui::SetNextWindowSize({ 475, 140 });
 		isFirstFrame = FALSE;
 	}
+
 	ImGui::Begin("Obfuscation Engine", NULL, 
 		ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoCollapse);
 
-	static CHAR appFilePath[MAX_PATH], pdbFilePath[MAX_PATH];
+	static std::string appFilePath, pdbFilePath;
 
 	ImGui::Text("Application");
 	ImGui::SetNextItemWidth(394.00f);
-	ImGui::InputText("###Application", appFilePath, MAX_PATH);
+	ImGui::InputText("##APP_INPUT", ( CHAR* )appFilePath.c_str(), MAX_PATH);
 	ImGui::SameLine();
 	ImGui::Dummy({ -13.00f, 0.00f });
 	ImGui::SameLine();
-	if (ImGui::Button("Open File###OPEN_APP"))
-	{
-		strcpy(appFilePath, Utils::fileDialogBox().c_str());
-
-		std::string Copy = appFilePath;
-		Copy.resize(Copy.size() - 3);
-		Copy += "pdb";
-
-		Obfuscator::addLog("Application added => " + std::string(appFilePath));
-		if (std::filesystem::exists(Copy))
-		{
-			Obfuscator::addLog("PDB found => " + Copy);
-			strcpy(pdbFilePath, Copy.c_str());
-		}
-
-	}
-
+	if (ImGui::Button("Open File##APP_BUTTON"))
+		appFilePath = Utils::fileDialogBox();
+		
 	ImGui::Text("PDB");
 	ImGui::SetNextItemWidth(394.00f);
-	ImGui::InputText("###PDB_INPUT", pdbFilePath, MAX_PATH);
+	ImGui::InputText("##PDB_INPUT", ( CHAR * )pdbFilePath.c_str(), MAX_PATH);
 	ImGui::SameLine();
 	ImGui::Dummy({ -13.00f, 0.00f });
 	ImGui::SameLine();
-	if (ImGui::Button("Open File###OPEN_PDB"))
-	{
-		strcpy(pdbFilePath, Utils::fileDialogBox().c_str());
-		Obfuscator::addLog("PDB added => " + std::string(pdbFilePath));
-	}
+	if (ImGui::Button("Open File##PDB_BUTTON"))
+		pdbFilePath = Utils::fileDialogBox();
 	
 	if (ImGui::Button("Run", { 460, 20 }))
-	{
-		BOOLEAN appExists = std::filesystem::exists(appFilePath);
-		if (!appExists)
-			Obfuscator::addLog("Application doesn't exists.");
+		Obfuscator::Run(appFilePath, pdbFilePath);
 
-		BOOLEAN pdbExists = std::filesystem::exists(pdbFilePath);
-		if (!pdbExists)
-			Obfuscator::addLog("PDB doesn't exists.");
-
-		if (appExists && pdbExists)
-		{
-			Obfuscator::Run(appFilePath, pdbFilePath);
-			justRanObfuscator = TRUE;
-		}
-	}
-
-	ImGui::Dummy({ 0.00f, 35.00f });
-
-	ImGui::Text("Logs:");
-	ImGui::BeginChild("Logs", { 460, 334 }, TRUE, 
-		ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoCollapse);
-
-	ImGui::PushStyleColor(ImGuiCol_Text, { 0.80f, 0.80f, 0.80f, 1.00f });
-	for (SIZE_T i = 0; i < Obfuscator::Logs.size(); ++i)
-		ImGui::Text(Obfuscator::Logs[i].c_str());
-	ImGui::PopStyleColor();
-	
-	if (justRanObfuscator)
-	{
-		ImGui::SetScrollHereY(1.00f);
-		justRanObfuscator = FALSE;
-	}
-		
-	ImGui::EndChild();
 	ImGui::End();
 }
 
--- Obfuscator.hpp
+++ Obfuscator.hpp
@@ -1,22 +1,7 @@
 namespace Obfuscator
 {
-	std::vector<std::string> Logs;
-	VOID addLog(std::string Message)
-	{
-		if (ImGui::CalcTextSize( Message.c_str() ).x > 420.00f)
-		{
-			while (ImGui::CalcTextSize(Message.c_str()).x > 420.00f)
-				Message.resize(Message.size() - 1);
-
-			Message += "...";
-		}
-
-		Logs.push_back( Message );
-	}
-	
 	BOOLEAN Run(std::string fileName, std::string pdbFileName)
 	{
-		Obfuscator::addLog("\n========================|Obfuscating|========================");
 		std::chrono::time_point startTime = std::chrono::steady_clock::now();
 
 		PE Image{ fileName.c_str()};
@@ -27,14 +12,12 @@ namespace Obfuscator
 		CZydisParser zydisParser{};
 
 		for (const CRoutine& Routine : pdbParser.Routines)
-		{
 			Virtualization::processRoutine(Image, zydisParser, Routine);
-		}
-		addLog(std::format("Parsed {} routines.\n", pdbParser.Routines.size()));
 
 		IMAGE_SECTION_HEADER* stubSection = Image.createSection(".stub", 512, 0x60000020);
 		Image.Refresh();
-		addLog(std::format("Created .stub section.\n", pdbParser.Routines.size()));
+
+
 
 
 
@@ -42,8 +25,7 @@ namespace Obfuscator
 		std::chrono::time_point endTime = std::chrono::steady_clock::now();
 		
 		LONG64 timeTaken = std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime).count();
-		addLog(std::format("=========================|{:.2f}ms|=========================", timeTaken / 1000.00f));
-
+	
 		Image.fileData.clear();
 		return TRUE;
 	}
