bfcbe3173a89ac54bbf87cc96e7f69ef16f8729c
Virtu4lizer <brbiproj@gmail.com>
2023-02-08 21:12:29 +0100

    jump encoder

--- ZydisInstruction.hpp
+++ ZydisInstruction.hpp
@@ -15,12 +15,32 @@ public:
 	{
 	}
 
-	BOOLEAN isJmpRelative() const
+	BOOLEAN isJumpRelative() const
 	{
 		if (!(Decoded.attributes & ZYDIS_ATTRIB_IS_RELATIVE))
 			return FALSE;
 
 		return Decoded.mnemonic >= ZYDIS_MNEMONIC_JB
 			&& Decoded.mnemonic <= ZYDIS_MNEMONIC_JZ;
+	}
+	std::vector<ZyanU8> encodeImmJumpWithOffset( ZyanU64 newOffset)
+	{
+		if (isJumpRelative())
+			return {};
+		ZydisEncoderRequest req;
+		memset(&req, 0, sizeof(req));
+		req.mnemonic = Decoded.mnemonic; // the jump could be a conditional jump, like JE, JB, JNZ, JZ etc... this is the whole point of this
+		req.machine_mode = ZYDIS_MACHINE_MODE_LONG_64;
+		req.operand_count = 1;
+		req.operands[0].type = ZYDIS_OPERAND_TYPE_IMMEDIATE;
+		req.operands[0].imm.u = newOffset;
+
+		std::vector<ZyanU8> encodedInstruction(ZYDIS_MAX_INSTRUCTION_LENGTH);
+		ZyanUSize encodedLength;
+
+		if (ZYAN_FAILED(ZydisEncoderEncodeInstruction(&req, encodedInstruction.data(), &encodedLength)))
+			return {};
+		encodedInstruction.resize(encodedLength);
+		return encodedInstruction;
 	}
 };
